b1b48a290729c9053b249d679a6cfbb59608fc9d
DachiMkh <mrdachi007@gmail.com>
2024-05-02 03:09:04 +0400

    update and history add

--- transfer.py
+++ transfer.py
@@ -1,17 +1,23 @@
-# მომხმარებლებს შეუძლიათ ფულის გადარიცხვა ორ ანგარიშს შორის, გადარიცხვამდე უნდა 
-# შემოწმდეს ანგარიშების ვალიდურობა და გადამცემის საკმარისი ბალანსი.
-
 from client_validator import check_client_info
-import client_main
+import client_main as main
 
 def money_transfer(giver, getter, amount):
-    giver = check_client_info(giver)
-    getter = check_client_info(getter)
-    if giver and getter:
-        if giver.get("balance") >= amount:
-            print("Proccessing....")
-            giver['balance'] -= amount
-            getter['balance'] += amount
-            client_main.record.append({"bank_no": giver, "bank_no": getter, "balance": amount})
+    giver_record = check_client_info(giver)
+    getter_record = check_client_info(getter)
+
+    if giver_record and getter_record:
+        giver_balance = float(giver_record.get("balance"))
+        getter_balance = float(getter_record.get("balance"))
+
+        if giver_balance >= amount:
+            print("Processing....")
+            giver_balance -= amount
+            getter_balance += amount
+            giver_record['balance'] = giver_balance
+            getter_record['balance'] = getter_balance
+            main.record.append({"bank_no": giver, "bank_no": getter, "balance": amount})
+            print("Transfer completed successfully.")
+        else:
+            print("Insufficient balance in giver's account.")
     else:
-        print("Does not exist")
+        print("One or both of the accounts do not exist.")
